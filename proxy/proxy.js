1906177d7b8818a6b2f133869e294729c62fd580
jaydin-1 <47401182+jaydin-1@users.noreply.github.com>
2023-03-05 15:17:55 -0700

    replic for server halfworking no gossip msg TS yet

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -4,36 +4,15 @@ const PORT = 2222;
 
 // Proxy middleware
 const { createProxyMiddleware } = require('http-proxy-middleware');
-// const target = 'http://localhost:1111'; // target host (Server URL)
-// const proxy = createProxyMiddleware({
-//   target,
-//   changeOrigin: true,
-
-//   onProxyReq: function onProxyReq(proxyReq, req, res) {
-//     console.log(`Forwarding ${req.method} ${req.path} request to server..`);
-// },
-// });
-
-
-// const targets = ['http://localhost:1111', 'http://localhost:1112', 'http://localhost:1113']; // target hosts (replica servers URLs)
-
-// target hosts (replica servers) and weights
-const targets = [
-  { target: 'http://localhost:1111', weight: 1 }, { target: 'http://localhost:1112', weight: 2 }, { target: 'http://localhost:1113', weight: 3 }
-]; 
-let totalWeight = targets.reduce((acc, curr) => acc + curr.weight, 0); // total weight of all replicas
-let currentIndex = 0;
-
-
-// creates a separate proxy for each target hosts
-const proxies = targets.map(target => createProxyMiddleware({
-  target: target.target,
+const target = 'http://localhost:1111'; // target host (Server URL)
+const proxy = createProxyMiddleware({
+  target,
   changeOrigin: true,
 
   onProxyReq: function onProxyReq(proxyReq, req, res) {
     console.log(`Forwarding ${req.method} ${req.path} request to server..`);
-  },
-}));
+},
+});
 
 // allow cross-origin requests
 app.use((req, res, next) => {
@@ -48,35 +27,8 @@ app.use((req, res, next) => {
 //     res.status(204).send();
 // });
 
-// // Start proxy server on port
-// app.use('/', proxy);
-
-// // Round-robin load balancing to distribute requests among replicas
-// let index = 0;
-// app.use('/', (req, res, next) => {
-//   proxies[index](req, res, next);
-//   index = (index + 1) % proxies.length;
-// });
-
-// weighted round-robin load balancing to distribute requests among replicas
-app.use('/', (req, res, next) => {
-  let weight = 0;
-
-  for (let i = currentIndex; i < targets.length; i++) {
-    const target = targets[i];
-    weight += target.weight;
-    // if weight is greater than total weight, then set currentIndex to target server index
-    if (weight >= totalWeight) {
-      currentIndex = i;
-      break;
-    }
-    // if end of target array, start from the beginning
-    if (i === targets.length - 1) {
-      i = -1;
-    }
-  }  
-  proxies[currentIndex](req, res, next);
-});
+// Start proxy server on port
+app.use('/', proxy);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- server/server.js
+++ server/server.js
@@ -16,8 +16,128 @@ const downloadFile = (name) => api.get(`/download?name=${name}`);
 
 let replicaNodes = ['http://localhost:1111', 'http://localhost:1112', 'http://localhost:1113'];
 
+// Replica Manager Object to handle replication
+const replicaManager = {
+    replicas:[],
+    init: function() {
+        replicaNodes.forEach((node) => {
+            this.replicas.push({url: node, state: 'active'});
+        });
+    },
+    // Replicate file to all active replicas
+    replicate: function(name, file) {
+        this.replicas.forEach((replica) => {
+            // Only replicate to active replicas
+            if (replica.state === 'active') {
+                // Send POST request to replica with file data and name as query parameter 
+                axios.post(`${replica.url}/upload?name=${name}`, {file:file}, {headers: {'content-type': 'application/json'}}).then (response => {
+                    console.log('Replication successful for replica: ' + replica.url);
+                }).catch(err => {
+                    // If replication fails, set replica state to inactive
+                    console.log('Replication failed for replica: ' + replica.url);
+                    replica.state = 'inactive';
+                });
+            }
+        }, this);
+    },
+    // Delete file from all active replicas
+    deleteReplica: function(name) {
+        // Iterate through all replicas
+        this.replicas.forEach((replica) => {
+            // Only delete from active replicas
+            if (replica.state === 'active') {
+                // Send POST request to replica with name as query parameter
+                axios.post(`${replica.url}/delete?name=${name}`).then (response => {
+                    console.log('Deletion successful for replica: ' + replica.url);
+                }).catch(err => {
+                    // If deletion fails, set replica state to inactive
+                    console.log('Deletion failed for replica: ' + replica.url);
+                    replica.state = 'inactive';
+                });
+            }
+        }, this);
+    },
+    // Download file from active replica and return file data
+    gossip: function() {
+        // Select random replica from replica list and send GET request to /state endpoint
+        let replica = this.replicas[Math.floor(Math.random() * this.replicas.length)];
+        axios.get('${replica.url}/state').then(response => {
+            let updatedReplicas = {
+                url: replica.url,
+                state: response.data.state
+            };
+        // Find index of replica in replica list and update state
+        let index = this.replicas.findIndex((replica) => replica.url === updatedReplicas.url);
+        this.replicas[index] = updatedReplicas;
+        // If replica is inactive, replicate all files to replica
+        if (updatedReplicas.state === 'inactive') {
+            console.log('Replica ' + updatedReplicas.url + ' is inactive');
+            this.replicateAll();
+        }
+        }).catch(err => {
+            console.log('Gossip failed for replica: ' + replica.url);
+        });
+    },
+    // Replicate all files to inactive replica
+    replicateAll: function() {
+        let activeReplicas = this.replicas.filter((replica) => replica.state === 'active');
+        let files = [];
+        // Iterate through all active replicas and get list of files
+        for (let i = 0; i < activeReplicas.length; i++) {
+            // Send GET request to replica and add files to files array
+            axios.get(`${activeReplicas[i].url}/files`).then(response => {
+                files = files.concat(response.data.files);
+                // If last active replica, iterate through all inactive replicas and replicate all files
+                if (i === activeReplicas.length - 1) {
+                    for (let j = 0; j < this.replicas.length; j++) {
+                        if (this.replicas[j].state === 'inactive') {
+                            // Iterate through all files and replicate to replica
+                            for (let k = 0; k < files.length; k++) {
+                                // Send POST request to replica with file data and name as query parameter
+                                axios.post(`${this.replicas[j].url}/upload?name=${files[k].name}`, {file:files[k].file}, {headers: {'content-type': 'application/json'}}).then (response => {
+                                    console.log('Replication successful for replica: ' + this.replicas[j].url);
+                                    this.replicas[j].state = 'active';
+                                }).catch(err => {
+                                    console.log('Replication failed for replica: ' + this.replicas[j].url);
+                                });
+                            }
+                        }
+                    }
+                }
+            }).catch(err => {
+                console.log('Replication failed for replica: ' + activeReplicas[i].url);
+            });
+        }
+    },
+    // Get random active replica from replica list
+    getReplica: function() {
+        let replica = this.replicas[Math.floor(Math.random() * this.replicas.length)];
+        if (replica.state === 'active') {
+            return replica;
+        } else {
+            return this.getReplica();
+        }
+    }
+};
 
+    // getReplica: function() {
+    //     let index = Math.floor(Math.random() * this.replicas.length);
+    //     return this.replicas[index];
+    // }
 
+// }
+
+    //         this.replicas = this.replicas.map((replica) => {
+    //             if (replica.url === updatedReplicas.url) {
+    //                 return updatedReplicas;
+    //             } else {
+    //                 return replica;
+    //             }
+    //         });
+    //     }).catch(err => {
+    //         console.log('Gossip failed for replica: ' + replica.url);
+    //     });
+    // },
 
 // // allow cross-origin requests (only required in proxy.js)
 // app.use((req, res, next) => {
@@ -26,6 +146,8 @@ let replicaNodes = ['http://localhost:1111', 'http://localhost:1112', 'http://lo
 //     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 // });
+
+replicaManager.init();
 
 // retreive all files from db
 app.get('/files', (req, res) => {
@@ -36,6 +158,29 @@ app.get('/files', (req, res) => {
     })
     .catch((err) => console.log(err));
 });
+
+// // delete file from db and replicate to all replicas
+// app.delete('/delete', (req, res) => {
+//     console.log(`Forwarding ${req.method} ${req.path} request to mongodb..`);
+//     deleteFile(req.query.name)
+//     .then((response) => {
+//         replicaManager.replicate(req.query.name, null);
+//         res.json(response.data);
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+
+// // add new file to db and replicate to all replicas
+// app.post('/upload', (req, res) => {
+//     console.log(`Forwarding ${req.method} ${req.path} request to mongodb..`);
+//     uploadFile(req.query.name, req.body.file)
+//     .then((response) => {
+//         replicaManager.replicate(req.query.name, req.body.file);
+//         res.json(response.data);
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 // add new file to db
 app.post('/upload', (req, res) => {
