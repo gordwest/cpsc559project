c5bd584ee4b189e65a0b6b951398b4206497ce75
jaydin-1 <47401182+jaydin-1@users.noreply.github.com>
2023-03-05 08:58:45 -0700

    +weightRR. todo:gossip thus  improve FaultTolor

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -15,10 +15,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // });
 
 
-const targets = ['http://localhost:1111', 'http://localhost:1112', 'http://localhost:1113']; // target hosts (replica servers URLs)
+// const targets = ['http://localhost:1111', 'http://localhost:1112', 'http://localhost:1113']; // target hosts (replica servers URLs)
+
+// target hosts (replica servers) and weights
+const targets = [
+  { target: 'http://localhost:1111', weight: 1 }, { target: 'http://localhost:1112', weight: 2 }, { target: 'http://localhost:1113', weight: 3 }
+]; 
+let totalWeight = targets.reduce((acc, curr) => acc + curr.weight, 0); // total weight of all replicas
+let currentIndex = 0;
+
+
 // creates a separate proxy for each target hosts
 const proxies = targets.map(target => createProxyMiddleware({
-  target,
+  target: target.target,
   changeOrigin: true,
 
   onProxyReq: function onProxyReq(proxyReq, req, res) {
@@ -42,11 +51,31 @@ app.use((req, res, next) => {
 // // Start proxy server on port
 // app.use('/', proxy);
 
-// Round-robin load balancing to distribute requests among replicas
-let index = 0;
+// // Round-robin load balancing to distribute requests among replicas
+// let index = 0;
+// app.use('/', (req, res, next) => {
+//   proxies[index](req, res, next);
+//   index = (index + 1) % proxies.length;
+// });
+
+// weighted round-robin load balancing to distribute requests among replicas
 app.use('/', (req, res, next) => {
-  proxies[index](req, res, next);
-  index = (index + 1) % proxies.length;
+  let weight = 0;
+
+  for (let i = currentIndex; i < targets.length; i++) {
+    const target = targets[i];
+    weight += target.weight;
+    // if weight is greater than total weight, then set currentIndex to target server index
+    if (weight >= totalWeight) {
+      currentIndex = i;
+      break;
+    }
+    // if end of target array, start from the beginning
+    if (i === targets.length - 1) {
+      i = -1;
+    }
+  }  
+  proxies[currentIndex](req, res, next);
 });
 
 app.listen(PORT, () => {
