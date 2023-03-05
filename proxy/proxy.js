716cc107de0a8233a90636066fc49a87aad16ff0
jaydin-1 <47401182+jaydin-1@users.noreply.github.com>
2023-03-05 06:12:30 -0700

    Temp:RR load-balancing - 1client->1proxy->replicas

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -4,15 +4,25 @@ const PORT = 2222;
 
 // Proxy middleware
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const target = 'http://localhost:1111'; // target host (Server URL)
-const proxy = createProxyMiddleware({
+// const target = 'http://localhost:1111'; // target host (Server URL)
+// const proxy = createProxyMiddleware({
+//   target,
+//   changeOrigin: true,
+
+//   onProxyReq: function onProxyReq(proxyReq, req, res) {
+//     console.log(`Forwarding ${req.method} ${req.path} request to server..`);
+// },
+// });
+
+const targets = ['http://localhost:1111', 'http://localhost:1112', 'http://localhost:1113']; // target hosts (replica servers URLs)
+const proxies = targets.map(target => createProxyMiddleware({
   target,
   changeOrigin: true,
 
   onProxyReq: function onProxyReq(proxyReq, req, res) {
     console.log(`Forwarding ${req.method} ${req.path} request to server..`);
-},
-});
+  },
+}));
 
 // allow cross-origin requests
 app.use((req, res, next) => {
@@ -27,8 +37,15 @@ app.use((req, res, next) => {
 //     res.status(204).send();
 // });
 
-// Start proxy server on port
-app.use('/', proxy);
+// // Start proxy server on port
+// app.use('/', proxy);
+
+// Round-robin load balancing to distribute requests among replicas
+let index = 0;
+app.use('/', (req, res, next) => {
+  proxies[index](req, res, next);
+  index = (index + 1) % proxies.length;
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
